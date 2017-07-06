7e212c6c24cf770fca0f170e7164c552f05d9412
John Kennerley <john@totalaoc.com>
2017-07-06 15:22:41 +0100

    ava tests are going, but how do you tests the vanilla web component.

--- wc/Wc.TestsJs/demo.test.js
+++ wc/Wc.TestsJs/demo.test.js
@@ -1,26 +1,15 @@
 ﻿import test from 'ava';
 
-//import { demo } from './src/demo-module';
 import { demo } from '../wc/components/dist-ts/mash-grid/demo-module';
-
-import { mashgrid} from '../wc/components/dist-ts/mash-grid/mash-grid';
-
 
 test('can add numbers', t => {
     t.is(1 + 1, 2);
 });
 
-
-
-
 test('can import from demo module', t => {
     const expected = 'a';
-
     const result = demo();
-
-    console.log(typeof demo);
+    //console.log(typeof demo);
     t.is(result, expected);
-
-    t.is(1,1)
-
+    t.is(1, 1);
 });
