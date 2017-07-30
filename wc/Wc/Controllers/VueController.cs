1dc307420b70ff07b5aa80003a5be5f6d516a5d1
johnkennerley <john.kennerley@gmail.com>
2017-07-30 20:11:23 +0100

    npm install  axios --save

---
+++ wc/Wc/Controllers/VueController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Wc.Controllers
+{
+    public class VueController : Controller
+    {
+        public ActionResult index()
+        {
+            return View();
+        }
+
+        public ActionResult VueHelloWorld()
+        {
+            return View();
+        }
+
+
+    }
+}
