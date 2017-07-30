4332886c685fb0e0ccbbb5c4247882db29395ba9
johnkennerley <john.kennerley@gmail.com>
2017-07-30 13:39:10 +0100

    ...

---
+++ wc/Wc/Controllers/Polymer.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace Wc.Controllers
+{
+    public class PolymerController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+
+
+        }
+    }
+}
+
